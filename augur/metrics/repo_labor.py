8418c47d6550ded19eb06e49cb064b005ac6b04a
ac25ville <55512495+ac25ville@users.noreply.github.com>
2021-05-02 18:19:03 -0500

    Updatedrepo_labor

--- augur/metrics/repo_labor.py
+++ augur/metrics/repo_labor.py
@@ -14,6 +14,13 @@ def get_labor(self, repo_id=None, repo_name, programming_language):
     if not repo_name:
         repo_name = 'repo_labor'
     
+    laborNewSQlGetParams = s.sql.text("""
+
+            
+
+    """
+    )
+
     laborNewSQL = s.sql.text("""
         SELECT C.repo_id,
         C.repo_name,
@@ -58,6 +65,6 @@ def get_labor(self, repo_id=None, repo_name, programming_language):
         programming_language;
         """)
     results = pd.read_sql(laborNewSQL, self.database, 
-            params={'repo_id' = repo_id, 'repo_name' = repo_name, 
-            'programming_language' = programming_language})
+            params={'repo_id': repo_id, 'programming_language': "Python"})
+    results = ["WE DID IT"]
     return results
